5f4d27a6ccdf83dc8085009dfde9cc6f3bdcc269
Nigel Ramsay <nigel.ramsay@abletech.co.nz>
2014-06-03 15:05:41 +1200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wellrailed::Application.config.secret_token = '0ff955860c07dcd04447fdcad500c60ae7a0e0dc816b672fd0c237c6ced1f08c075b1b13cdec81a04c065faffcdedb7302a3e1c42885a67e562ba72cc6b7f54b'
